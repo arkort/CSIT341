c268fc5fc4260d2c4fc26ee2be5c86bedce5b6ad
stalcker23 <a-zenit-a@rambler.ru>
2016-02-23 17:33:02 +0300

    1_TASK

---
+++ Акимов/Task1/ADO.NET_1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ADO.NET_1
+{
+    class Program
+    {
+        static Dictionary<string, int> counter = new Dictionary<string, int>();
+        static List<Tuple<int, string>> copy = new List<Tuple<int, string>>();
+        static int allFiles = 0;
+
+        static void Directory(string name)
+        {
+            DirectoryInfo dir = new DirectoryInfo(name);
+            Record(dir);
+            foreach (var item in dir.EnumerateDirectories())
+            {
+                Directory(item.FullName);
+            }
+            
+        }
+         static IEnumerable<FileInfo> Record(DirectoryInfo name)
+        {
+            var array = name.EnumerateFiles();
+            foreach (var item in array)
+            {
+                if (counter.ContainsKey(Path.GetExtension(item.FullName)))
+                    counter[Path.GetExtension(item.FullName)]++;
+                else
+                    counter.Add(Path.GetExtension(item.FullName), 1);
+                allFiles++;
+                
+            }
+            return array;
+        }
+        static void Write()
+        {
+            foreach (var item in counter)
+            {
+                copy.Add(Tuple.Create(item.Value, item.Key));
+            }
+            copy.Sort();
+            copy.Reverse();
+            using (StreamWriter write = new StreamWriter("output.txt"))
+            {
+                foreach (var item in copy)
+                {
+                    write.WriteLine("<{0}>#<{1}>#<{2:F4}>", item.Item2, item.Item1, (double)item.Item1 / allFiles);
+                }
+
+            }
+        }
+        static void Main()
+        {
+            Directory(@"C:\Users\Артем\Documents\Visual Studio 2015\Projects");
+            Write();
+        }
+    }
+}
