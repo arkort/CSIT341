3adbda13102b06cda4cbeb79ec54c236d993ab69
Artyom <a-zenit-a@rambler.ru>
2016-03-02 12:28:29 +0300

    Task2_changes

--- Акимов/Task2/ADO.NET_2/ADO.NET_2/Program.cs
+++ Акимов/Task2/ADO.NET_2/ADO.NET_2/Program.cs
@@ -8,7 +8,7 @@ namespace ADO.NET_2
 {
     class Program
     {
-        static public bool flag=true;
+
         static void Main()
         {
             XmlReaderSettings booksSettings = new XmlReaderSettings();
@@ -17,49 +17,37 @@ namespace ADO.NET_2
             booksSettings.ValidationEventHandler += new ValidationEventHandler(booksSettingsValidationEventHandler);
             XmlReader books = XmlReader.Create(@"XMLFile1.xml", booksSettings);
 
-            while (books.Read()) { }
-            if (flag == true)
+            while (books.Read())
             {
-                Console.WriteLine("Файл корректен");
-                XmlTextReader reader = new XmlTextReader("XMLFile1.xml");
-                while (reader.Read())
+                switch (books.NodeType)
                 {
-                    switch (reader.NodeType)
-                    {
-
-
-                        case XmlNodeType.Element:
+                    case XmlNodeType.Element:
+                        {
+                            if (books.Name == "BOOK")
                             {
-                                if (reader.Name == "Bibl")
-                                {
-                                    continue;
-                                }
-                                if (reader.Name == "BOOK")
-                                {
-
-                                    Console.WriteLine(reader.Name);
-                                    Console.Write("Attribute: Title=" + reader.GetAttribute(0));
-                                    Console.WriteLine();
-
-                                    break;
-                                }
-                                else
-                                {
-                                    Console.Write("- " + reader.Name);
-                                }
+                                Console.WriteLine(books.Name);
+                                Console.Write("Attribute: Title=" + books.GetAttribute(0));
+                                Console.WriteLine();
                                 break;
                             }
-                        case XmlNodeType.Text:
-                            {
-                                Console.WriteLine(" " + reader.Value);
 
+                            else if  (books.Name != "Bibl")
+
+                            {
+                                Console.Write("- " + books.Name);
                             }
                             break;
+                        }
+                    case XmlNodeType.Text:
+                        {
+                            Console.WriteLine(" " + books.Value);
 
-                    }
+                        }
+                        break;
+
                 }
-                Console.ReadLine();
             }
+            
         }
         static void booksSettingsValidationEventHandler(object sender, ValidationEventArgs e)
         {
@@ -74,7 +62,7 @@ namespace ADO.NET_2
                 Console.Write("ERROR: ");
                 Console.WriteLine(e.Message);
             }
-            flag = false;
+            
         }
     }
 }
