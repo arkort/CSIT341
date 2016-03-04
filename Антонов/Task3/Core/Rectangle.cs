77d73dd65a4e98992af5509f27a4176ea2cd818e
ansergex <ansergex@ya.ru>
2016-03-04 12:28:45 +0300

    Add solution

---
+++ Антонов/Task3/Core/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Xml.Serialization;
+
+namespace Task3
+{
+    [Serializable]
+    [XmlType("rectangle")]
+    public class Rectangle : Figure
+    {
+        [XmlElement("Color")]
+        public override string FillColor { get; set; }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle() { }
+
+        public override void Draw(Canvas context)
+        {
+            Color color;
+
+            try
+            {
+                color = (Color)ColorConverter.ConvertFromString(FillColor);
+            }
+            catch (Exception)
+            {
+                color = (Color)ColorConverter.ConvertFromString("black");
+            }
+
+            var rect = new System.Windows.Shapes.Rectangle();
+            rect.Fill = new SolidColorBrush(color);
+            rect.Width = Width;
+            rect.Height = Height;
+
+            Canvas.SetLeft(rect, X);
+            Canvas.SetTop(rect, Y);
+
+            context.Children.Add(rect);
+        }
+    }
+}
