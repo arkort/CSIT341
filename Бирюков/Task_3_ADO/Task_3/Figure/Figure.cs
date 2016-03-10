a98cd2bfac6227dccf7bb9834cc2f3dc29d2ef77
ab245 <ab245@bk.ru>
2016-03-11 00:35:17 +0300

    Добавлено 3 задание

---
+++ Бирюков/Task_3_ADO/Task_3/Figure/Figure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Xml.Serialization;
+
+namespace Task_3.Figure
+{
+    [Serializable]
+    [XmlInclude(typeof(Circle))]
+    [XmlInclude(typeof(MyRectangle))]
+    [XmlInclude(typeof(Line))]
+    [XmlInclude(typeof(Triange))]
+    public abstract class AbstractFigure : IDrawable
+    {
+        public abstract void Draw(Graphics graphics);
+
+        protected AbstractFigure()
+        { }
+    }
+}
