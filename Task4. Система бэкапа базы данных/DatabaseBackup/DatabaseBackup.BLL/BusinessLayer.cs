f421b46d1ecad771cd755ec41edd099f2ae5be2f
ansergex <ansergex@ya.ru>
2016-04-05 21:12:52 +0300

    Initial commit

---
+++ Task4. Система бэкапа базы данных/DatabaseBackup/DatabaseBackup.BLL/BusinessLayer.cs
@@ -0,0 +1,27 @@
+﻿using DatabaseBackup.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseBackup.BLL
+{
+    public class BusinessLayer : IBusinessLayer
+    {
+        public void Backup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Connect(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Restore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
