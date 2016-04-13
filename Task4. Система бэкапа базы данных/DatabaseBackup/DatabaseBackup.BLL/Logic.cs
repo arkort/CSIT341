56328625a77188957383a598715e112a5f8b442d
rustam_s <asdf@noreply.com>
2016-04-13 13:40:38 +0300

    Cosmetic changes.

--- Task4. Система бэкапа базы данных/DatabaseBackup/DatabaseBackup.BLL/Logic.cs
+++ Task4. Система бэкапа базы данных/DatabaseBackup/DatabaseBackup.BLL/Logic.cs
@@ -12,28 +12,28 @@ namespace DatabaseBackup.BLL
 
         public void Backup(string address, string databaseName, string username, string password)
         {
-            dal.Backup($"Server={address};Database={databaseName};User Id={username};Password={password}");
+            this.dal.Backup($"Server={address};Database={databaseName};User Id={username};Password={password}");
         }
 
         public void BackupLocalInstance (string address, string databaseName)
         {
-            dal.Backup($@"Data Source={address};Initial Catalog=""{databaseName}"";Integrated Security=True");
+            this.dal.Backup($@"Data Source={address};Initial Catalog=""{databaseName}"";Integrated Security=True");
         }
 
 
         public void Restore(System.DateTime date)
         {
-            dal.Restore(date);
+            this.dal.Restore(date);
         }
 
-        public IEnumerable<string> ShowDatabases(string address, string username, string password)
+        public IEnumerable<string> ShowDatabases(string address, string username, string password="")
         {
-            return dal.ShowDatabases($"Server={address};Database=master;User Id=myUsername;Password=myPassword");
+            return password == "" ? this.dal.ShowDatabases($"Server={address};User Id=myUsername;") : this.dal.ShowDatabases($"Server={address};User Id=myUsername;Password=myPassword");
         }
 
         public IEnumerable<string> ShowDatabasesLocalInstance(string address)
         {
-            return dal.ShowDatabases($@"Data Source={address};Initial Catalog=master;Integrated Security=True");
+            return this.dal.ShowDatabases($@"Data Source={address};Integrated Security=True");
         }
     }
 }
