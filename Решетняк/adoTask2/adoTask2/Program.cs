9b25d8a6c0fe917e05730dae60aa68313af9f7e2
Eugine <HYDRA>
2016-04-20 13:08:09 +0300

    Task2 is done

---
+++ Решетняк/adoTask2/adoTask2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+
+class XmlSchemaSetExample
+{
+    class Phone
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+    static void Main()
+    {
+        XmlDocument xdoc = new XmlDocument();
+        xdoc.Load(@"F:\С#_project\adoTask2\adoTask2\XMLFile1.xml");
+        XmlReaderSettings booksSettings = new XmlReaderSettings();
+        booksSettings.Schemas.Add(null, @"F:\С#_project\adoTask2\adoTask2\XMLSchema1.xsd");
+        booksSettings.ValidationType = ValidationType.Schema;
+        booksSettings.ValidationEventHandler += new ValidationEventHandler(booksSettingsValidationEventHandler);
+
+        XmlReader reader = XmlReader.Create(@"F:\С#_project\adoTask2\adoTask2\XMLFile1.xml", booksSettings);
+
+        while (reader.Read())
+        {
+            switch (reader.NodeType)
+            {
+                case XmlNodeType.Element:
+                    {
+                        if (reader.Name == "PHONE")
+                        {
+                            Console.WriteLine(reader.Name);
+                            Console.Write("Attribute: Title=" + reader.GetAttribute(0));
+                            Console.WriteLine();
+                            break;
+                        }
+
+                        else if (reader.Name != "PHONES")
+
+                        {
+                            Console.Write("- " + reader.Name);
+                        }
+                        break;
+                    }
+                case XmlNodeType.Text:
+                    {
+                        Console.WriteLine(" " + reader.Value);
+
+                    }
+                    break;
+
+            }
+        }
+
+    }
+
+
+        
+    
+
+    static void booksSettingsValidationEventHandler(object sender, ValidationEventArgs e)
+    {
+        if (e.Severity == XmlSeverityType.Warning)
+        {
+            Console.Write("WARNING: ");
+            Console.WriteLine(e.Message);
+        }
+        else if (e.Severity == XmlSeverityType.Error)
+        {
+            Console.Write("ERROR: ");
+            Console.WriteLine(e.Message);
+        }
+    }   
+} 
