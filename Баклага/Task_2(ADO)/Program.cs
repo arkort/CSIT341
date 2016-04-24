abe32050cdb06379ad73255d93a8945534196395
BaklagaA <temkalpshinoda@mail.ru>
2016-04-25 01:18:45 +0300

    Task1 and Task2 added.

---
+++ Баклага/Task_2(ADO)/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+
+class XmlSchemaSetExample
+{
+    static void Main()
+    {
+        XmlReaderSettings carSettings = new XmlReaderSettings();
+        carSettings.Schemas.Add(null, @"C:\Users\Artem\Documents\Visual Studio 2015\Projects\Task_2(ADO)\XMLSchema.xsd");
+        carSettings.ValidationType = ValidationType.Schema;
+        carSettings.ValidationEventHandler += new ValidationEventHandler(booksSettingsValidationEventHandler);
+
+        XmlReader books = XmlReader.Create(@"C:\Users\Artem\Documents\Visual Studio 2015\Projects\Task_2(ADO)\XML_File.xml", carSettings);
+
+
+        while (books.Read())
+        {
+            switch (books.NodeType)
+            {
+                case XmlNodeType.Element:
+                    {
+                        if (books.Name == "Car")
+                        {
+                            Console.Write(" Cars Features " + books.GetAttribute(0));
+                            Console.WriteLine();
+                            break;
+                        }
+
+                        else if (books.Name != "Catalog")
+
+                        {
+                            Console.Write(" - " + books.Name);
+                        }
+                        break;
+                    }
+                case XmlNodeType.Text:
+                    {
+                        Console.WriteLine(":" + books.Value);
+
+                    }
+                    break;
+
+            }
+        }
+
+    }
+
+
+    static void booksSettingsValidationEventHandler(object sender, ValidationEventArgs e)
+    {
+        if (e.Severity == XmlSeverityType.Warning)
+        {
+            Console.Write("WARNING: ");
+            Console.WriteLine(e.Message);
+        }
+        else if (e.Severity == XmlSeverityType.Error)
+        {
+            Console.Write("ERROR: ");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+
