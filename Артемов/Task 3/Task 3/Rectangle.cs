44429ad258e17ed3699e4615902674bb3803f40d
Victor Artemov <victor_artemov@outlook.com>
2016-05-03 14:26:27 +0300

    Добавлено 3 задание

---
+++ Артемов/Task 3/Task 3/Rectangle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Task_3
+{
+    [Serializable]
+
+    public class Rectangle : Figure
+    {
+        private Point beginPoint;
+        private int width;
+        private int heigth;
+        
+        public Rectangle(Point beginPoint, int width, int heigth)
+        {
+            this.beginPoint = beginPoint;
+            this.width = width;
+            this.heigth = heigth;            
+        }
+        public Rectangle() { }
+
+        public Point BeginPoint
+        {
+            get
+            {
+                return this.beginPoint;
+            }
+            set
+            {
+                this.beginPoint = value;
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+        public int Heigth
+        {
+            get
+            {
+                return this.heigth;
+            }
+            set
+            {
+                this.heigth = value;
+            }
+        }
+                
+        public override void Draw(Graphics graphics)
+        {
+            graphics.DrawRectangle(Pens.Gold, beginPoint.X, beginPoint.Y, width, heigth);
+        }
+    }
+}
---
+++ Артемов/Task 3/Task 3/Triangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Task_3
+{
+    [Serializable]
+     public class Triangle : Figure
+    {
+        private Point a;
+        private Point b;
+        private Point c;
+
+        public Triangle(Point A, Point B, Point C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+        public Triangle() { }
+
+        public Point A
+        {
+            get
+            {
+                return this.a;
+            }
+            set
+            {
+                this.a = value;
+            }
+        }
+        public Point B
+        {
+            get
+            {
+                return this.b;
+            }
+            set
+            {
+                this.b = value;
+            }
+        }
+        public Point C
+        {
+            get
+            {
+                return this.c;
+            }
+            set
+            {
+                this.c = value;
+            }
+        }
+
+        public override void Draw(Graphics graphics)
+        {
+            graphics.DrawLine(new Pen(Color.Black), A, B);
+            graphics.DrawLine(new Pen(Color.Black), C, B);
+            graphics.DrawLine(new Pen(Color.Black), C, A);
+        }
+    }
+}
